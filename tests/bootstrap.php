4a1be4574d05b3e2d5b47a7eb85e34ccb444df0f
Jimmy Comack <jimmy@yoast.com>
2017-09-08 10:33:12 +0200

    Initial testing with new configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,18 +11,20 @@ if ( function_exists( 'xdebug_disable' ) ) {
 echo 'Welcome to the Yoast SEO Test Suite' . PHP_EOL;
 echo 'Version: 1.0' . PHP_EOL . PHP_EOL;
 
-if ( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
-	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+$plugin_dir = '../../';
+
+if ( getenv( 'WP_PLUGIN_DIR' ) !== false ) {
+	$plugin_dir = getenv( 'WP_PLUGIN_DIR' );
 }
 
+define( 'WP_PLUGIN_DIR', $plugin_dir );
 define( 'YOAST_ENVIRONMENT', 'production' );
-
 
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'wordpress-seo/wp-seo.php' ),
 );
 
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+if ( getenv( 'WP_DEVELOP_DIR' ) !== false ) {
 	require getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit/includes/bootstrap.php';
 }
 else {
